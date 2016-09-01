3c65b0c201a86c4a8faa0ea90df2b864bdf4acf3
Stephen Cleary <github.ourteddybear@xoxy.net>
2016-09-01 19:21:56 -0400

    Athena

---
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        // Debug me, but first follow the steps in the README.
+        static void Main(string[] args)
+        {
+            MainAsync().Wait();
+        }
+
+        static async Task MainAsync()
+        { 
+            Debugger.Break();
+
+            // I can step into this method just fine.
+            GitLinkTestLib.Test.SynchronousMethod();
+
+            // Hmmm, can't step into this method. :(
+            var task1 = GitLinkTestLib.Test.AsynchronousMethod();
+
+            await task1;
+
+            // This one I can step into just fine.
+            var task2 = GitLinkTestLib.Test.TaskReturningSynchronousMethod();
+
+            await task2;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+Debug this project, but first:
+
+1. `Tools` -> `Options` -> `Debugging` -> Uncheck `Enable Just My Code`
+2. `Tools` -> `Options` -> `Debugging` -> Check `Enable source server support`
+3. `Tools` -> `Options` -> `Debugging` -> `Symbols` -> Set `Cache symbols in this directory` to a local folder.
